3cb33a2456e93ad85a57721e4ff50b292fc2fd14
Béla Varga <netzzwerg@googlemail.com>
2014-05-31 23:53:28 +0200

    Refactoring to bower components. Added weblimejs module.

--- book.js
+++ book.js
@@ -1,6 +1,37 @@
-;(function(global, undefined) {
-'use strict';
+(function() {
+  'use strict';
 
-	console.log('book init');
+  requirejs.config({
+    baseUrl: 'bower_components',
+    paths: {
+      'jquery': 'jquery/jquery.min',
+      'ace': 'ace/lib/ace',
+      'ace/theme/default': 'weblimejs/lib/theme-default',
+      'ace/theme/letterpress': 'weblimejs/lib/theme-letterpress',
+      'weblime': 'weblimejs/lib/weblime'
+    }
+  });
 
-}(this));
+  require(['jquery', 'weblime'], function($, Weblime) {
+
+    new Weblime({
+      el: $('.weblime')[0],
+      theme: 'monokai',
+      path: 'source/',
+      callback: function(weblime) {
+        console.log(weblime);
+      }
+    });
+
+    new Weblime({
+      el: $('.weblime')[1],
+      theme: 'monokai',
+      path: 'source/',
+      callback: function(weblime) {
+        console.log(weblime);
+      }
+    });
+
+  });
+
+}());
