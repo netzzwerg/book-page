1d7823db6227ba95badad6b408a9b09bebf39698
Béla Varga <netzzwerg@googlemail.com>
2014-05-31 23:53:59 +0200

    Added javascript source files.

---
+++ source/01.js
@@ -0,0 +1,12 @@
+// A car "class"
+function Car( model ) {
+
+  this.model = model;
+  this.color = "silver";
+  this.year  = "2012";
+
+  this.getInfo = function () {
+    return this.model + " " + this.year;
+  };
+
+}
---
+++ source/02.js
@@ -0,0 +1,5 @@
+var myCar = new Car("ford");
+
+myCar.year = "2010";
+
+console.log( myCar.getInfo() );
